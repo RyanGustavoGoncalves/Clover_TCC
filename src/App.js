549219948803bb938731d4935e2052620df40bf4
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2023-10-12 22:28:34 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div className='imgFundo'></div>
+        <main>
+          <header>
+            <h1>Clover</h1>
+            <nav>
+              <ul>
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Produto</li>
+                <li>Contato</li>
+                <li>Login</li>
+              </ul>
+            </nav>
+          </header>
+          <article>
+            <section>
+              <h2>SUA PLATAFORMA<br/>DE ESTUDOS<br/>ORGANIZADA</h2>
+              <span>Nossa plataforma coloca o<br/>conhecimento ao seu alcance,<br/> adpatando-se ao seu estilo de<br/>aprendizado</span>
+            </section>
+            <div className='btn'>
+              <button className='button1'>Comece agora</button>
+              <button className='button2'>Saiba mais!</button>
+            </div>
+          </article>
+        </main>
+      </div>
   );
 }
 
